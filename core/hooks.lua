65498d1b7e1e04431709fadd671a32e180cd64a1
Antonio Lobato <aj.lobato@gmail.com>
2023-11-06 14:42:03 -0800

    removed debug messages

--- core/hooks.lua
+++ core/hooks.lua
@@ -4,9 +4,7 @@ local addonName = ...
 local addon = LibStub('AceAddon-3.0'):GetAddon(addonName)
 
 function addon:OpenAllBags(requesterFrame)
-  print("open")
 end
 
 function addon:ToggleAllBags(requesterFrame)
-  print("open")
 end
