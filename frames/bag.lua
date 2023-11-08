90e2d402f9295f357f946b4c228d6ea5c0ca947e
Antonio Lobato <aj.lobato@gmail.com>
2023-11-08 00:00:46 -0800

    More work on bag composition.

--- frames/bag.lua
+++ frames/bag.lua
@@ -6,11 +6,6 @@ local addon = LibStub('AceAddon-3.0'):GetAddon(addonName)
 ---@class BagFrame: AceModule
 local bagFrame = addon:NewModule('BagFrame')
 
----@class BagPool
----@field Acquire fun(): Frame
----@field Release fun(pool: BagPool, frame: Frame)
-local pool = CreateFramePool("Frame", nil, "BackdropTemplate")
-
 -------
 --- Bag Prototype
 -------
@@ -48,14 +43,31 @@ end
 function bagFrame:Create(kind)
   local b = {}
   setmetatable(b, { __index = bagProto })
-  local f = pool:Acquire()
+  -- TODO(lobato): Compose the entire frame here.
+
+  b.kind = kind
+  -- The main display frame for the bag.
+  ---@class Frame: BackdropTemplate
+  local f = CreateFrame("Frame", nil, nil, "BackdropTemplate")
+
   b.frame = f
+  b.frame:SetParent(UIParent)
   b.frame:Hide()
+  b.frame:SetSize(200, 200)
+  b.frame:SetPoint("CENTER")
+  b.frame:SetBackdropColor(0, 0, 0, 1)
+  b.frame:SetBackdrop({
+    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+    edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+    tile = true,
+    tileSize = 32,
+    edgeSize = 32,
+    insets = { left = 11, right = 12, top = 12, bottom = 11 }
+  })
   return b
 end
 
 --- Destroy destroys the given bag view.
 ---@param bag Bag
 function bagFrame:Destroy(bag)
-  pool:Release(bag.frame)
 end
