c0417c2fcd9828473c45a571417166adcdeb8392
Antonio Lobato <aj.lobato@gmail.com>
2023-11-09 14:44:10 -0800

    wip grid draw

--- frames/grid.lua
+++ frames/grid.lua
@@ -12,7 +12,8 @@ local cellProto = {}
 
 ---@class Grid
 ---@field frame Frame
----@field cells any[]
+---@field cells Cell|Section[]
+---@field maxCellWidth number
 local gridProto = {}
 
 function gridProto:Show()
@@ -37,6 +38,28 @@ function gridProto:Sort(fn)
   table.sort(self.cells, fn)
 end
 
+-- Draw will draw the grid.
+function gridProto:Draw()
+  local width = 0
+  local height = 0
+  for i, cell in ipairs(self.cells) do
+    cell.frame:ClearAllPoints()
+    if i == 1 then
+      cell.frame:SetPoint('TOPLEFT', self.frame, 'TOPLEFT', 0, 0)
+      width = width + cell.frame:GetWidth()
+      height = height + cell.frame:GetHeight()
+    elseif i % self.maxCellWidth == 0 then
+      cell.frame:SetPoint('TOPLEFT', self.cells[i - self.maxCellWidth].frame, 'BOTTOMLEFT', 0, 0)
+      height = height + cell.frame:GetHeight()
+    else
+      cell.frame:SetPoint('TOPLEFT', self.cells[i - 1].frame, 'TOPRIGHT', 0, 0)
+      width = width + cell.frame:GetWidth()
+    end
+  end
+  self.frame:SetWidth(width)
+  self.frame:SetHeight(height)
+end
+
 -- Create will create a new grid frame.
 ---@param parent Frame
 ---@return Grid
@@ -46,5 +69,9 @@ function grid:Create(parent)
   local f = CreateFrame('Frame', nil, nil, "BackdropTemplate")
   f:SetParent(parent)
   g.frame = f
+  g.cells = {}
+  g.maxCellWidth = 3
   return g
 end
+
+grid:Enable()
