351c2a7c188c5c3964566762c7efbcb6a94ab21a
Antonio Lobato <aj.lobato@gmail.com>
2023-11-21 15:17:48 -0800

    added comments

--- core/constants.lua
+++ core/constants.lua
@@ -19,6 +19,8 @@ const.BAG_KIND = {
   BANK = 1,
 }
 
+-- BANK_BAGS contains all the bags that are part of the bank, including
+-- the main bank view.
 const.BANK_BAGS = {
   [Enum.BagIndex.Bank] = Enum.BagIndex.Bank,
   [Enum.BagIndex.BankBag_1] = Enum.BagIndex.BankBag_1,
@@ -30,6 +32,8 @@ const.BANK_BAGS = {
   [Enum.BagIndex.BankBag_7] = Enum.BagIndex.BankBag_7,
 }
 
+-- BANK_ONLY_BAGS contains all the bags that are part of the bank, excluding
+-- the main bank view.
 const.BANK_ONLY_BAGS = {
   [Enum.BagIndex.BankBag_1] = Enum.BagIndex.BankBag_1,
   [Enum.BagIndex.BankBag_2] = Enum.BagIndex.BankBag_2,
@@ -40,10 +44,13 @@ const.BANK_ONLY_BAGS = {
   [Enum.BagIndex.BankBag_7] = Enum.BagIndex.BankBag_7,
 }
 
+-- REAGENTBANK_BAGS contains the reagent bank bag.
 const.REAGENTBANK_BAGS = {
   [Enum.BagIndex.Reagentbank] = Enum.BagIndex.Reagentbank,
 }
 
+-- BACKPACK_BAGS contains all the bags that are part of the backpack, including
+-- the main backpack bag.
 const.BACKPACK_BAGS = {
   [Enum.BagIndex.Backpack] = Enum.BagIndex.Backpack,
   [Enum.BagIndex.Bag_1] = Enum.BagIndex.Bag_1,
@@ -53,6 +60,8 @@ const.BACKPACK_BAGS = {
   [Enum.BagIndex.ReagentBag] = Enum.BagIndex.ReagentBag,
 }
 
+-- BACKPACK_ONLY_BAGS contains all the bags that are part of the backpack, excluding
+-- the main backpack bag.
 const.BACKPACK_ONLY_BAGS = {
   [Enum.BagIndex.Bag_1] = Enum.BagIndex.Bag_1,
   [Enum.BagIndex.Bag_2] = Enum.BagIndex.Bag_2,
