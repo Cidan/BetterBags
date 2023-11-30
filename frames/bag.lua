8bdccb8a6f5883f020d44d931089e0faee6ad33e
Antonio Lobato <aj.lobato@gmail.com>
2023-11-29 17:21:27 -0800

    Made resize handle a bit bigger.
    Auto hide/show the resize handle.

--- frames/bag.lua
+++ frames/bag.lua
@@ -297,7 +297,7 @@ function bagFrame:Create(kind)
   -- Create the money frame only in the player backpack bag.
   if kind == const.BAG_KIND.BACKPACK then
     local moneyFrame = money:Create()
-    moneyFrame.frame:SetPoint("BOTTOMRIGHT", bottomBar, "BOTTOMRIGHT", 0, 0)
+    moneyFrame.frame:SetPoint("BOTTOMRIGHT", bottomBar, "BOTTOMRIGHT", -10, 0)
     moneyFrame.frame:SetParent(b.frame)
     b.moneyFrame = moneyFrame
   end
@@ -327,7 +327,7 @@ function bagFrame:Create(kind)
   -- Create the bag content frame.
   local content = grid:Create(b.frame)
   content:GetContainer():SetPoint("TOPLEFT", leftHeader, "BOTTOMLEFT", 3, -3)
-  content:GetContainer():SetPoint("BOTTOMRIGHT", b.frame, "BOTTOMRIGHT", -3, 3)
+  content:GetContainer():SetPoint("BOTTOMRIGHT", b.frame, "BOTTOMRIGHT", -3, 30)
   content.compactStyle = const.GRID_COMPACT_STYLE.NONE
   content:Show()
   b.content = content
--- views/listview.lua
+++ views/listview.lua
@@ -168,6 +168,7 @@ function views:ListView(bag, dirtyItems)
     h = 40
   end
   bag.content:ShowScrollBar()
+  --TODO(lobato): Load this from the database.
   bag.frame:SetWidth(600)
   bag.frame:SetHeight(700)
 end
