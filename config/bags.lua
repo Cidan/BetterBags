77eaaf80a88e575b08ffc2635a60bab4c1b73d16
Antonio Lobato <aj.lobato@gmail.com>
2024-01-20 14:32:24 -0800

    Added item sorting to config menu

--- config/bags.lua
+++ config/bags.lua
@@ -28,6 +28,7 @@ function config:GetBagOptions(kind)
         type = "multiselect",
         name = L:G("Categories"),
         desc = L:G("Select which categories to show in this bag. If an option is checked, items that belong to the checked category will be put into a section for that category."),
+        order = 1,
         get = function(_, value)
           return DB:GetCategoryFilter(kind, value)
         end,
@@ -51,6 +52,7 @@ function config:GetBagOptions(kind)
         type = "select",
         name = L:G("Item Compaction"),
         desc = L:G("If Simple is selected, item sections will be sorted from left to right, however if a section can fit in the same row as the section above it, the section will move up."),
+        order = 2,
         style = "radio",
         get = function()
           return DB:GetBagCompaction(kind)
@@ -70,6 +72,7 @@ function config:GetBagOptions(kind)
         type = "select",
         name = L:G("Section Sorting"),
         desc = L:G("Select how sections should be sorted."),
+        order = 3,
         style = "radio",
         get = function()
           return DB:GetSectionSortType(kind, DB:GetBagView(kind))
@@ -84,7 +87,28 @@ function config:GetBagOptions(kind)
           [const.SECTION_SORT_TYPE.SIZE_DESCENDING] = L:G("Size Descending"),
           [const.SECTION_SORT_TYPE.SIZE_ASCENDING] = L:G("Size Ascending"),
         }
-      }
+      },
+
+      itemSorting = {
+        type = "select",
+        name = L:G("Item Sorting"),
+        desc = L:G("Select how items should be sorted."),
+        order = 4,
+        style = "radio",
+        get = function()
+          return DB:GetItemSortType(kind, DB:GetBagView(kind))
+        end,
+        set = function(_, value)
+          DB:SetItemSortType(kind, DB:GetBagView(kind), value)
+          config:GetBag(kind):Wipe()
+          config:GetBag(kind):Refresh()
+        end,
+        values = {
+          [const.ITEM_SORT_TYPE.QUALITY_THEN_ALPHABETICALLY] = L:G("Quality, then Alphabetically"),
+          [const.ITEM_SORT_TYPE.ALPHABETICALLY_THEN_QUALITY] = L:G("Alphabetically, then Quality"),
+        }
+      },
+
     }
   }
   return options
