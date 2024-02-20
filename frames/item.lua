70fe85f90f3b73d1b4dcecde946605596b60e8fc
Antonio Lobato <aj.lobato@gmail.com>
2024-02-19 17:58:32 -0800

    Fixed a bug where selling items very quickly could cause lock/unlock bugs.

--- frames/item.lua
+++ frames/item.lua
@@ -174,6 +174,7 @@ end
 function itemFrame.itemProto:Lock()
   if self.data.isItemEmpty or self.data.basic then return end
   local itemLocation = ItemLocation:CreateFromBagAndSlot(self.data.bagid, self.data.slotid)
+  if itemLocation == nil or not itemLocation:IsValid() then return end
   C_Item.LockItem(itemLocation)
   self.data.itemInfo.isLocked = true
   SetItemButtonDesaturated(self.button, self.data.itemInfo.isLocked)
@@ -185,6 +186,7 @@ end
 function itemFrame.itemProto:Unlock()
   if self.data.isItemEmpty or self.data.basic then return end
   local itemLocation = ItemLocation:CreateFromBagAndSlot(self.data.bagid, self.data.slotid)
+  if itemLocation == nil or not itemLocation:IsValid() then return end
   C_Item.UnlockItem(itemLocation)
   self.data.itemInfo.isLocked = false
   SetItemButtonDesaturated(self.button, self.data.itemInfo.isLocked)
