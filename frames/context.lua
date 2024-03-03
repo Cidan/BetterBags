c3cf047ed4b98b2c56623636ba92636ef0b22848
Antonio Lobato <aj.lobato@gmail.com>
2024-03-03 13:49:04 -0800

    Fixed a bug where bag view wouldn't collapse.

--- frames/context.lua
+++ frames/context.lua
@@ -142,6 +142,7 @@ function context:CreateContextMenu(bag)
             database:SetPreviousView(bag.kind, const.BAG_VIEW.ONE_BAG)
           else
             database:SetBagView(bag.kind, const.BAG_VIEW.ONE_BAG)
+            database:SetPreviousView(bag.kind, const.BAG_VIEW.ONE_BAG)
             events:SendMessage('bags/FullRefreshAll')
           end
         end
@@ -162,6 +163,7 @@ function context:CreateContextMenu(bag)
           if database:GetBagView(bag.kind) == const.BAG_VIEW.SECTION_ALL_BAGS then
             database:SetPreviousView(bag.kind, const.BAG_VIEW.SECTION_GRID)
           else
+            database:SetPreviousView(bag.kind, const.BAG_VIEW.SECTION_GRID)
             database:SetBagView(bag.kind, const.BAG_VIEW.SECTION_GRID)
             events:SendMessage('bags/FullRefreshAll')
           end
@@ -183,6 +185,7 @@ function context:CreateContextMenu(bag)
           if database:GetBagView(bag.kind) == const.BAG_VIEW.SECTION_ALL_BAGS then
             database:SetPreviousView(bag.kind, const.BAG_VIEW.LIST)
           else
+            database:SetPreviousView(bag.kind, const.BAG_VIEW.LIST)
             database:SetBagView(bag.kind, const.BAG_VIEW.LIST)
             events:SendMessage('bags/FullRefreshAll')
           end
