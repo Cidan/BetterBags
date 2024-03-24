3fac06d1d4df55e1e91beb963c660b1a17677e81
Antonio Lobato <aj.lobato@gmail.com>
2024-03-24 10:02:25 -0700

    Fixed a bug with an internal stacks variable being doubled

--- data/items.lua
+++ data/items.lua
@@ -475,7 +475,6 @@ function items:BackpackLoadFunction()
           data.stackedCount = data.itemInfo.currentItemCount
           data.stacks = 0
           stackItem.stackedCount = stackItem.stackedCount + data.itemInfo.currentItemCount
-          stackItem.stacks = stackItem.stacks + 1
           if not self:IsNewItem(stackItem) or not self:IsNewItem(data) then
             self:ClearNewItem(data)
             self:ClearNewItem(stackItem)
@@ -615,7 +614,6 @@ function items:BankLoadFunction()
           data.stackedCount = data.itemInfo.currentItemCount
           data.stacks = 0
           stackItem.stackedCount = stackItem.stackedCount + data.itemInfo.currentItemCount
-          stackItem.stacks = stackItem.stacks + 1
           if not self:IsNewItem(stackItem) or not self:IsNewItem(data) then
             self:ClearNewItem(data)
             self:ClearNewItem(stackItem)
