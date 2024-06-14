93eaec965607b717600c60bbc8146877603e0180
Antonio Lobato <aj.lobato@gmail.com>
2024-06-14 09:21:58 -0700

    Added close button to simple dark.

--- themes/simpledark.lua
+++ themes/simpledark.lua
@@ -38,37 +38,18 @@ local simpleDark = {
       title:SetHeight(30)
       decoration.title = title
 
+      local close = CreateFrame("Button", nil, decoration, "UIPanelCloseButtonNoScripts")
+      close:SetPoint("TOPRIGHT", decoration, "TOPRIGHT", 1, 0)
+      close:SetScript("OnClick", function()
+        frame.Owner:Hide()
+      end)
+
       themes.SetupBagButton(frame.Owner, decoration --[[@as Frame]])
       -- Save the decoration frame for reuse.
       decoratorFrames[frame:GetName()] = decoration
     else
       decoration:Show()
     end
-    --frame.NineSlice:Hide()
-    --frame.Bg:Hide()
-    --frame.TopTileStreaks:Hide()
-    --frame.PortraitContainer.CircleMask:SetTexture([[Interface\Common\Common-IconMask]])
-    --frame.PortraitContainer.CircleMask:SetPoint("TOPLEFT", frame.PortraitContainer.portrait, "TOPLEFT", 2, -2)
-    --frame.PortraitContainer.CircleMask:SetPoint("BOTTOMRIGHT", frame.PortraitContainer.portrait, "BOTTOMRIGHT", -2, 1)
-    --frame:SetPortraitTextureSizeAndOffset(30, 4, -7)
-    --frame.Backdrop:SetBackdrop({
-    --  bgFile = 'Interface\\ChatFrame\\ChatFrameBackground',
-    --  edgeFile = 'Interface\\Tooltips\\UI-Tooltip-Border',
-    --  edgeSize = 16,
-    --  insets = {left = 4, right = 4, top = 4, bottom = 4}
-    --})
-    --frame.Backdrop:SetBackdropColor(0, 0, 0, 1)
-    --frame.Backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-    --frame.Backdrop:Show()
-    --frame.TitleContainer.TitleText:SetFont(UNIT_NAME_FONT, 12, "")
-    --frame.TitleContainer.TitleText:SetTextColor(1, 1, 1)
-    --frame.TitleContainer:Show()
-    --frame.PortraitContainer:Show()
-    --frame.Owner.sideAnchor:ClearAllPoints()
-    --frame.Owner.sideAnchor:SetPoint("TOPRIGHT", frame, "TOPLEFT")
-    --frame.Owner.sideAnchor:SetPoint("BOTTOMRIGHT", frame, "BOTTOMLEFT")
-    --themes:resetCloseButton(frame.CloseButton)
-    --frame.CloseButton:Show()
   end,
   Simple = function(frame)
     --frame.NineSlice:Hide()
