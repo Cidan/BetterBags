16a4f0ed5a9aadf4822a509b327d7165f65a1851
Antonio Lobato <aj.lobato@gmail.com>
2024-06-26 11:28:25 -0700

    Added a small fix for potentially bad itemsperrow numbers being set.

--- core/database.lua
+++ core/database.lua
@@ -426,6 +426,21 @@ function DB:Migrate()
       }
     end
   end
+
+  -- Fix the column count and items per row values from a previous bug.
+  -- Do not remove before Q1'25.
+  for _, bagView in pairs(const.BAG_VIEW) do
+    for _, bagKind in pairs(const.BAG_KIND) do
+      if DB.data.profile.size[bagView] then
+        local t = DB.data.profile.size[bagView][bagKind]
+        if t then
+          if t.itemsPerRow ~= nil and t.itemsPerRow > 30 or t.itemsPerRow < 1 then
+            t.itemsPerRow = 7
+          end
+        end
+      end
+    end
+  end
 end
 
 DB:Enable()
