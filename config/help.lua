e9d32191af9f9dee2ad6924341f44e04478513ba
Antonio Lobato <aj.lobato@gmail.com>
2024-07-29 13:34:56 -0700

    Updated help for search

--- config/help.lua
+++ config/help.lua
@@ -83,12 +83,48 @@ function config:CreateAllHelp()
   title = L:G("How do I search for items?"),
   text = L:G([[
     You can bind a key to open the search bar in the World of Warcraft keybindings menu. You can also open the search bar by shift clicking on the bag button at the top of the bag frame.
-    Once the search bar is open, you can type in the name of an item to search for it. You can also use the following search operators:
-    - `type:` to search for items of a specific type (e.g. `type:weapon`)
-    - `subtype:` to search for items of a specific subtype (e.g. `subtype:axe`)
-    - `name:` to search for items with a specific name (e.g. `name:axe`)
-    - `gear:` to search for items that are part of an equipment set (e.g. `gear:tank`)
-    - `exp:` to search for items from a specific expansion (e.g. `exp:shadowlands`)
+    Once the search bar is open, you can type in the name of an item to search for it.
+    The BetterBags search engine is extremly comprehensive and supports many different search operators and grouping.
+    You can search for items by a number of fields, such as name, type, subtype, expansion, and more, or combine multiple fields to create complex searches.
+
+    Logical Operators include:
+    - `AND` to combine multiple search terms (e.g. `axe AND sword`)
+    - `OR` to search for items that match any of the search terms (e.g. `axe OR sword`)
+    - `NOT` to exclude items that match a search term (e.g. `NOT axe`)
+    - `(` and `)` to group search terms (e.g. `(axe OR sword) AND epic`)
+
+    Comparison Operators include:
+    - `>` to search for items with a value greater than a number (e.g. `level > 10`)
+    - `<` to search for items with a value less than a number (e.g. `level < 10`)
+    - `=` to search for items with a value equal to a number or text (e.g. `level = 10 or name = axe`)
+    - `>=` to search for items with a value greater than or equal to a number (e.g. `level >= 10`)
+    - `<=` to search for items with a value less than or equal to a number (e.g. `level <= 10`)
+
+    The following fields are supported:
+    - `name` to search for items by name (e.g. `name = axe`)
+    - `type` to search for items by type (e.g. `type = weapon`)
+    - `subtype` to search for items by subtype (e.g. `subtype = axe`)
+    - `quality` to search for items by quality (e.g. `quality = epic`)
+    - `equipmentLocation` to search for items by equipment location (e.g. `equipmentLocation = head`)
+    - `expansion` to search for items by expansion (e.g. `expansion = classic`)
+    - `level` to search for items by level (e.g. `level > 10`)
+    - `rarity` to search for items by rarity (e.g. `rarity = rare`)
+    - `id` to search for items by id (e.g. `id = 12345`)
+    - `stackCount` to search for items by stack count (e.g. `stackCount > 10`)
+    - `class` to search for items by classID (e.g. `class = 10`)
+    - `subclass` to search for items by subclassID (e.g. `subclass = 10`)
+    - `bagid` to search for items by bagID (e.g. `bagid = 0`)
+    - `slotid` to search for items by slotID (e.g. `slotid = 0`)
+    - `reagent` to search for items by reagent (e.g. `reagent = true`)
+    - `bound` to search for items by bound status (e.g. `bound = true`)
+    - `quest` to search for items by quest status (e.g. `quest = true`)
+    - `activeQuest` to search for items by active quest status (e.g. `activeQuest = true`)
+  
+    For rarity, you can use numerical comparison operators on rarity names, for example:
+    - `rarity = poor`
+    - `rarity > common`
+    - `rarity >= uncommon`
+    - ...etc
     
     For expansions, you can use the following expansions shortcuts (e.g. `exp:classic`):
     - `classic`
