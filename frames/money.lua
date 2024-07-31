4ef7a5839836301ccca141701eb7e3e44099fee6
Antonio Lobato <aj.lobato@gmail.com>
2024-07-30 17:42:30 -0700

    Updated the money deposit frame to offer better visuals and tooltips, and make it easier to click.

--- frames/money.lua
+++ frames/money.lua
@@ -15,6 +15,7 @@ local events = addon:GetModule('Events')
 
 ---@class Money
 ---@field frame Frame
+---@field overlay Frame
 ---@field copperButton Button
 ---@field silverButton Button
 ---@field goldButton Button
@@ -44,45 +45,15 @@ function money:Create(warbank)
   ---@type Money
   local m = setmetatable({}, { __index = money.moneyProto })
   m.warbank = warbank
-
-  local f = CreateFrame("Frame", addonName .. "MoneyFrame", UIParent)
+  local name = addonName .. "MoneyFrame" .. (warbank and "Warbank" or "")
+  local f = CreateFrame("Frame", name, UIParent)
+  local overlay = CreateFrame("Frame", name.."overlay", f)
   m.frame = f
-  m.frame:SetSize(128,18)
-  m.copperButton = self:CreateButton("copper", m.frame, warbank)
-  m.silverButton = self:CreateButton("silver", m.copperButton, warbank)
-  m.goldButton = self:CreateButton("gold", m.silverButton, warbank)
-  m.frame:Show()
-
-  m:Update()
-  events:RegisterEvent("PLAYER_MONEY", function()
-    m:Update()
-  end)
-  return m
-end
-
-
----@param kind string
----@param parent Frame
----@param warbank? boolean
----@return Button
-function money:CreateButton(kind, parent, warbank)
-  local b = CreateFrame("Button", nil, parent)
-  b:SetSize(32, 13)
-  if kind == "copper" then
-    b:SetPoint("RIGHT", 0, 0)
-  else
-    b:SetPoint("RIGHT", parent, "LEFT", -4, 0)
-  end
-  b:RegisterForClicks("LeftButtonUp", "RightButtonUp")
-  b:SetNormalAtlas("coin-" .. kind)
-  b:GetNormalTexture():ClearAllPoints()
-  b:GetNormalTexture():SetPoint("RIGHT", 0, 0)
-  b:GetNormalTexture():SetSize(13, 13)
-  local fs = b:CreateFontString(nil, "OVERLAY")
-  b:SetFontString(fs)
-  b:SetNormalFontObject("Number12Font")
-  fs:SetPoint("RIGHT", -13, 0)
-  b:SetScript("OnClick", function(_, button)
+  m.overlay = overlay
+  overlay:SetAllPoints()
+  overlay:SetAlpha(0.5)
+  overlay:EnableMouse(true)
+  overlay:SetScript("OnMouseDown", function(_, button)
     if button == "LeftButton" then
       if warbank then
         StaticPopup_Show("BANK_MONEY_WITHDRAW", nil, nil, {bankType = Enum.BankType.Account})
@@ -95,6 +66,61 @@ function money:CreateButton(kind, parent, warbank)
       end
     end
   end)
+
+  overlay:SetScript("OnEnter", function()
+    GameTooltip:SetOwner(overlay, "ANCHOR_TOP", 0, 5)
+    if warbank then
+      GameTooltip:AddDoubleLine("Left Click", "Withdraw money", 1, 0.81, 0, 1, 1, 1)
+      GameTooltip:AddDoubleLine("Right Click", "Deposit money", 1, 0.81, 0, 1, 1, 1)
+    else
+      GameTooltip:AddDoubleLine("Left Click", "Pick up money", 1, 0.81, 0, 1, 1, 1)
+    end
+    GameTooltip:Show()
+  end)
+
+  overlay:SetScript("OnLeave", function()
+    GameTooltip:Hide()
+  end)
+
+  local t = overlay:CreateTexture(nil, "HIGHLIGHT")
+  t:SetTexture("Interface\\QuestFrame\\UI-QuestTitleHighlight")
+  t:SetBlendMode("ADD")
+  t:SetAllPoints()
+
+  m.frame:SetSize(128,18)
+  m.copperButton = self:CreateButton("copper", m.frame)
+  m.silverButton = self:CreateButton("silver", m.copperButton)
+  m.goldButton = self:CreateButton("gold", m.silverButton)
+  m.frame:Show()
+
+  m:Update()
+  events:RegisterEvent("PLAYER_MONEY", function()
+    m:Update()
+  end)
+  return m
+end
+
+
+---@param kind string
+---@param parent Frame
+---@return Button
+function money:CreateButton(kind, parent)
+  local b = CreateFrame("Button", nil, parent)
+  b:EnableMouse(false)
+  b:SetSize(32, 13)
+  if kind == "copper" then
+    b:SetPoint("RIGHT", 0, 0)
+  else
+    b:SetPoint("RIGHT", parent, "LEFT", -4, 0)
+  end
+  b:SetNormalAtlas("coin-" .. kind)
+  b:GetNormalTexture():ClearAllPoints()
+  b:GetNormalTexture():SetPoint("RIGHT", 0, 0)
+  b:GetNormalTexture():SetSize(13, 13)
+  local fs = b:CreateFontString(nil, "OVERLAY")
+  b:SetFontString(fs)
+  b:SetNormalFontObject("Number12Font")
+  fs:SetPoint("RIGHT", -13, 0)
   b:Show()
   return b
 end
