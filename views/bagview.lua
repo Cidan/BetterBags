c655325a84ba0e3b5ad56de396e511daf425dcda
Antonio Lobato <aj.lobato@gmail.com>
2024-08-05 22:24:29 -0700

    In-frame Bag Search (#546)
    * The in-frame bag bar is now much bigger and set within the frame for all built-in themes.
    
    * You can now create themes using the in-bag bag bar.

--- views/bagview.lua
+++ views/bagview.lua
@@ -128,6 +128,16 @@ local function AddSlot(view, newSlotKey)
 end
 
 ---@param view View
+local function UpdateViewSize(view)
+  local parent = view.content:GetContainer():GetParent()
+  if database:GetInBagSearch() then
+    view.content:GetContainer():SetPoint("TOPLEFT", parent, "TOPLEFT", const.OFFSETS.BAG_LEFT_INSET, const.OFFSETS.BAG_TOP_INSET - 20)
+  else
+    view.content:GetContainer():SetPoint("TOPLEFT", parent, "TOPLEFT", const.OFFSETS.BAG_LEFT_INSET, const.OFFSETS.BAG_TOP_INSET)
+  end
+end
+
+---@param view View
 ---@param ctx Context
 ---@param bag Bag
 ---@param slotInfo SlotInfo
@@ -216,6 +226,9 @@ local function BagView(view, ctx, bag, slotInfo)
   if h == 0 then
     h = 40
   end
+  if database:GetInBagSearch() then
+    h = h + 20
+  end
   view.content:HideScrollBar()
   --TODO(lobato): Implement SafeSetSize that prevents the window from being larger
   -- than the screen space.
@@ -224,6 +237,7 @@ local function BagView(view, ctx, bag, slotInfo)
   const.OFFSETS.BAG_BOTTOM_INSET + -const.OFFSETS.BAG_TOP_INSET +
   const.OFFSETS.BOTTOM_BAR_HEIGHT + const.OFFSETS.BOTTOM_BAR_BOTTOM_INSET
   bag.frame:SetHeight(bagHeight)
+  UpdateViewSize(view)
 end
 
 ---@param parent Frame
