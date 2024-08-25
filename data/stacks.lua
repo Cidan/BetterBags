142bad557b9533eb086e907e7809ee437976f3cc
Antonio Lobato <aj.lobato@gmail.com>
2024-08-25 13:38:35 -0700

    Fixed a bug when adding empty items to a stack.
    Empty items can no longer be indexed or removed from search.

--- data/stacks.lua
+++ data/stacks.lua
@@ -18,68 +18,71 @@ local stack = {}
 --- Creates a new stack
 ---@return Stack
 function stacks:Create()
-    local newState = setmetatable({}, {__index = stack})
-    newState.stacksByItemHash = {}
-    return newState
+  local newState = setmetatable({}, {__index = stack})
+  newState.stacksByItemHash = {}
+  return newState
 end
 
 --- Initializes or updates stack information for an item
 ---@param item ItemData
 function stack:AddToStack(item)
-    local itemHash = item.itemHash
-    local slotkey = item.slotkey
+  local itemHash = item.itemHash
+  local slotkey = item.slotkey
+  if item.isItemEmpty then
+    return
+  end
 
-    if not self.stacksByItemHash[itemHash] then
-        self.stacksByItemHash[itemHash] = {count = 1, rootItem = slotkey, slotkeys = {}}
-        return
-    end
+  if not self.stacksByItemHash[itemHash] then
+    self.stacksByItemHash[itemHash] = {count = 1, rootItem = slotkey, slotkeys = {}}
+    return
+  end
 
-    local stackinfo = self.stacksByItemHash[itemHash]
+  local stackinfo = self.stacksByItemHash[itemHash]
 
-    if not stackinfo.slotkeys[slotkey] then
-        stackinfo.slotkeys[slotkey] = true
-        stackinfo.count = stackinfo.count + 1
-    end
+  if not stackinfo.slotkeys[slotkey] then
+    stackinfo.slotkeys[slotkey] = true
+    stackinfo.count = stackinfo.count + 1
+  end
 end
 
 --- Removes an item from a stack
 ---@param item ItemData
 function stack:RemoveFromStack(item)
-    local itemHash = item.itemHash
-    local slotkey = item.slotkey
-    local stackinfo = self.stacksByItemHash[itemHash]
-    if not stackinfo then return end
+  local itemHash = item.itemHash
+  local slotkey = item.slotkey
+  local stackinfo = self.stacksByItemHash[itemHash]
+  if not stackinfo then return end
 
-    if stackinfo.rootItem == slotkey then
-        if next(stackinfo.slotkeys) then
-            stackinfo.rootItem = next(stackinfo.slotkeys)
-            stackinfo.slotkeys[stackinfo.rootItem] = nil
-            stackinfo.count = stackinfo.count - 1
-        else
-            self.stacksByItemHash[itemHash] = nil
-        end
-    elseif stackinfo.slotkeys[slotkey] then
-        stackinfo.slotkeys[slotkey] = nil
-        stackinfo.count = stackinfo.count - 1
+  if stackinfo.rootItem == slotkey then
+    if next(stackinfo.slotkeys) then
+      stackinfo.rootItem = next(stackinfo.slotkeys)
+      stackinfo.slotkeys[stackinfo.rootItem] = nil
+      stackinfo.count = stackinfo.count - 1
+    else
+      self.stacksByItemHash[itemHash] = nil
     end
+  elseif stackinfo.slotkeys[slotkey] then
+    stackinfo.slotkeys[slotkey] = nil
+    stackinfo.count = stackinfo.count - 1
+  end
 end
 
 --- Gets the total count of an item across all stacks
 ---@param itemHash string
 ---@return number
 function stack:GetTotalCount(itemHash)
-    local stackinfo = self.stacksByItemHash[itemHash]
-    return stackinfo and stackinfo.count or 0
+  local stackinfo = self.stacksByItemHash[itemHash]
+  return stackinfo and stackinfo.count or 0
 end
 
 --- Gets stack information for an item
 ---@param itemHash string
 ---@return StackInfo?
 function stack:GetStackInfo(itemHash)
-    return self.stacksByItemHash[itemHash]
+  return self.stacksByItemHash[itemHash]
 end
 
 --- Clears all stack information
 function stack:Clear()
-    wipe(self.stacksByItemHash)
+  wipe(self.stacksByItemHash)
 end
