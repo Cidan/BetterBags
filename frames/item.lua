24bbbaf4f954ccc38b3447d5cb3b1e208c842e1c
zeptognome <zeptognome@gmail.com>
2024-09-03 19:35:24 -0700

    classic compat
    Signed-off-by: zeptognome <zeptognome@gmail.com>

--- frames/item.lua
+++ frames/item.lua
@@ -298,7 +298,7 @@ end
 function itemFrame.GetItemContextMatchResult(item)
   local itemLocation = ItemLocation:CreateFromBagAndSlot(item.bagID, item:GetID())
   if not itemLocation then return ItemButtonUtil.ItemContextMatchResult.DoesNotApply end
-  if not itemLocation:IsValid() then return ItemButtonUtil.ItemContextMatchResult.DoesNotApply end
+  if not C_Item.DoesItemExist(itemLocation) then return ItemButtonUtil.ItemContextMatchResult.DoesNotApply end
   if not itemLocation:IsBagAndSlot() then return ItemButtonUtil.ItemContextMatchResult.DoesNotApply end
   local result = ItemButtonUtil.GetItemContextMatchResultForItem( itemLocation ) --[[@as integer]]
   if not const.BACKPACK_BAGS[item.bagID] then return ItemButtonUtil.ItemContextMatchResult.Match end
