b623ed51f70c54cd91c9cb6780351874ed1db678
Antonio Lobato <aj.lobato@gmail.com>
2026-02-02 10:29:30 -0800

    undo column fix

--- frames/grid.lua
+++ frames/grid.lua
@@ -312,7 +312,7 @@ function gridProto:calculateColumns(options)
       if rowWidth + cell.frame:GetWidth() > options.maxWidthPerRow then
         if currentHeight + cell.frame:GetHeight() > splitAt then
           currentColumn = currentColumn + 1
-          currentHeight = cell.frame:GetHeight()
+          currentHeight = 0
         else
           currentHeight = currentHeight + cell.frame:GetHeight()
         end
