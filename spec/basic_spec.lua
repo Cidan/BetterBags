c92de50cfcb2182ab3b1d18bc78344774e9fe666
Antonio Lobato <aj.lobato@gmail.com>
2026-02-20 14:05:02 -0800

    feat: We need to start building out the Busted tests for the Be... (#897)
    Automated commit generated by the Nanomite code tool.
    
    Prompt:
    We need to start building out the Busted tests for the BetterBags WoW addon. The first step is getting `AceAddon-3.0` to load in our tests without errors, as it is used everywhere in the addon. Please do the following: 1. Create or update an `install-deps.sh` script to check out `AceAddon-3.0` (and any of its dependencies, like `LibStub` and `CallbackHandler-1.0`) using the `svn checkout` command. Place them in a directory suitable for testing (like a `libs` or `.libs` folder). 2. Set up the ...

--- spec/basic_spec.lua
+++ spec/basic_spec.lua
@@ -1,5 +1,14 @@
-describe("basic dummy test", function()
-  it("should pass", function()
-    assert.is_true(true)
+describe("AceAddon-3.0 Test", function()
+  it("should load LibStub and AceAddon-3.0 successfully", function()
+    -- LibStub is globally registered by WoW convention (and mocked in setup)
+    assert.is_not_nil(LibStub)
+    
+    local AceAddon = LibStub("AceAddon-3.0", true)
+    assert.is_not_nil(AceAddon)
+    
+    -- Test that we can create a simple addon
+    local myAddon = AceAddon:NewAddon("TestAddon")
+    assert.is_not_nil(myAddon)
+    assert.are.equal("TestAddon", myAddon:GetName())
   end)
 end)
