3b522c8f55e141ead89836e69b6d262ed18011ea
Antonio Lobato <aj.lobato@gmail.com>
2026-02-20 22:00:59 -0800

    clean up old files (#905)

--- test_args.lua
+++
@@ -1,8 +0,0 @@
-local function fn(...)
-    print("Args:", select("#", ...))
-    for i=1, select("#", ...) do
-        print(i, (select(i, ...)))
-    end
-    print("Select 2:", select(2, ...))
-end
-fn("event", "ctx", "arg1", "arg2")
